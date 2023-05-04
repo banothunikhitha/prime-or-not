f721f76b7b1a962559ca5b9cb6565f4e9e820c72
banothunikhitha <132464892+banothunikhitha@users.noreply.github.com>
2023-05-04 21:46:03 +0530

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,20 @@
+import unittest
+from prime import is_prime
+
+class TestPrime(unittest.TestCase):
+
+    def test_prime(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertTrue(is_prime(5))
+        self.assertTrue(is_prime(7))
+        self.assertTrue(is_prime(11))
+        self.assertTrue(is_prime(13))
+        self.assertFalse(is_prime(4))
+        self.assertFalse(is_prime(6))
+        self.assertFalse(is_prime(8))
+        self.assertFalse(is_prime(9))
+        self.assertFalse(is_prime(10))
+        self.assertFalse(is_prime(12))
+        self.assertFalse(is_prime(14))
+        self.assertFalse(is_prime(15))
